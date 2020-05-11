90c62eaff6d7798e257a1339029016ef9d5c8607
Stephan Mantler <step@stephanmantler.com>
2020-05-11 22:09:59 +0000

    first shot at a custom SVG icon

--- src/blocks/map/index.js
+++ src/blocks/map/index.js
@@ -31,7 +31,54 @@ export const name = 'stepman/super-map-block';
 export const settings = {
 	title: __('Super Map Block', 'super-map-block'),
 	description: __('Embed a leaflet map element', 'super-map-block'),
-	icon: 'admin-site-alt3',
+//	icon: 'admin-site-alt3',
+/*<!-- <style>
+	.st0 { fill: none; stroke:#000000;stroke-width:0.9377;stroke-miterlimit:10;}
+	.st1 { fill: none; stroke:#000000;stroke-width:3;stroke-miterlimit:10;}
+	.st2 { fill:#ACF5F9;}
+	.st3 { fill:#FFFFFF;stroke:#000000;stroke-width:4;stroke-miterlimit:10;}
+</style> --> */
+	icon: { src: ( <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+	 viewBox="0 0 256 256">
+<g id="Layer_1_1_">
+	<polygon fill="none" stroke="#888" strokeWidth="10" points="226.8,201.3 173.7,211.1 120.5,204.9 67.3,211.1 14.2,201.3 27.5,148.2 14.2,95 27.5,41.8 80.6,51.6 
+		133.8,45.4 187,51.6 240.1,41.8 226.8,95 240.1,148.2 	"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="14.2" y1="95" x2="67.3" y2="104.8"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="27.5" y1="148.2" x2="80.6" y2="158"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="80.6" y1="51.6" x2="67.3" y2="104.8"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="80.6" y1="158" x2="67.3" y2="211.1"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="120.5" y1="204.9" x2="133.8" y2="151.8"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="133.8" y1="45.4" x2="120.5" y2="98.6"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="187" y1="51.6" x2="173.7" y2="104.8"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="173.7" y1="211.1" x2="187" y2="158"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="226.8" y1="95" x2="173.7" y2="104.8"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="67.3" y1="104.8" x2="120.5" y2="98.6"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="133.8" y1="151.8" x2="187" y2="158"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="173.7" y1="104.8" x2="120.5" y2="98.6"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="67.3" y1="104.8" x2="80.6" y2="158"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="133.8" y1="151.8" x2="120.5" y2="98.6"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="173.7" y1="104.8" x2="187" y2="158"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="80.6" y1="158" x2="133.8" y2="151.8"/>
+	<line stroke="#000"  fill="none" strokeWidth="1.5" x1="240.1" y1="148.2" x2="187" y2="158"/>
+	<polygon stroke="#000"  fill="none" strokeWidth="1.5" points="27.7,41.9 80.6,51.6 133.8,45.4 186.6,51.8 240.1,41.8 226.8,95 240.1,148.2 227.2,201.5 173.7,211.1 
+		120.5,204.9 67.7,211.4 13.5,201.5 27.4,148.6 14.2,95 	"/>
+</g>
+<g id="Layer_2_1_">
+	<g>
+		<polygon fill="#ACF5D9" stroke="#000" strokeWidth="1.65" points="16.8,92.3 41.6,55 213.5,55 238.4,92.3 127.6,203.1 		"/>
+		<path stroke="#000" strokeWidth="3" d="M210,61.7l19.8,29.8L127.4,193.8L25,91.4l19.8-29.8h82.4H210 M217.1,48.4h-89.5H37.9L8.2,93.2l119.4,119.4L247,93.2
+			L217.1,48.4L217.1,48.4z"/>
+	</g>
+	<g>
+		<path fill="#fff" stroke="#000" strokeWidth="10" d="M76.3,151.3l19-22.7c11.6,8.8,24.8,12.4,37.2,12.4c6.5,0,9.1-1.7,9.1-4.6v-0.2c0-3-3.3-4.8-14.7-6.9
+			c-23.8-4.8-44.8-11.7-44.8-34.2v-0.3c0-20.2,15.9-35.9,45.2-35.9c20.5,0,35.7,4.8,48.1,14.6l-17.4,24C147.9,90.1,136,87,126.1,87
+			c-5.3,0-7.8,1.8-7.8,4.5v0.3c0,2.8,2.8,4.8,14.2,6.8c27.3,5,45.5,12.9,45.5,34.4v0.3c0,22.3-18.4,36.1-47,36.1
+			C109.2,169.2,89.7,163.1,76.3,151.3z"/>
+	</g>
+</g>
+</svg> )
+		
+	},
 	category: 'embed',
 	example: {},
 	attributes: {
